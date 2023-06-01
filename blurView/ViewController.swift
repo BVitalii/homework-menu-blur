7a30c673bca24fe2ec68b79c718c96dc5a695249
Vitali <i@borodulinvm.ru>
2023-06-01 18:32:36 +0300

    синяя кнопка чтоб проверить эффект блюр

--- blurView/ViewController.swift
+++ blurView/ViewController.swift
@@ -8,12 +8,53 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var countTap = 0
+    var mainManuView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .systemYellow
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    @IBOutlet weak var leadingConstraint: NSLayoutConstraint!
+    @IBOutlet weak var blurViewButtom: UIButton!
+    @IBOutlet weak var blurView: UIVisualEffectView!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        title = "Меню"
+        navigationItem.leftBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "list.dash"), style: .done, target: self, action: #selector(barButtonPressed))
+        
+        blurView.isHidden = true
+        view.addSubview(mainManuView)
+        
     }
-
-
+    
+    
+    @IBAction func blurViewButtomPressed(_ sender: UIButton) {
+        self.leadingConstraint.constant -= 250
+        
+        UIView.animate(withDuration: 0.3){
+            self.view.layoutIfNeeded()
+        }
+        blurView.isHidden = true
+        countTap -= 1
+    }
+    
+    @objc func barButtonPressed(_: Any){
+        while countTap != 1 {
+            self.leadingConstraint.constant += 250
+            countTap += 1
+        }
+        UIView.animate(withDuration: 0.3){
+            self.view.layoutIfNeeded()
+        }
+        blurView.isHidden = false
+    }
 }
+
+
 
